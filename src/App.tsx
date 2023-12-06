2b07e5eaab8a75327add9f9f923c48bfa360f846
Elias Gomes <eliasgo221@gmail.com>
2023-12-05 22:16:40 -0300

    first commit: simple setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalCss } from './styles'
+
+import Rotas from './routes'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <>
+      <BrowserRouter>
+        <GlobalCss />
+        <Rotas />
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
